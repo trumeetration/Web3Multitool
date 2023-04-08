4089ffe0a0497bfdb55fb7a4df442e7574b0e78b
trum <war.naruto@mail.ru>
2023-04-08 15:58:12 +0500

    [+] AccountInfo model

---
+++ LayerzeroMultitool/Models/AccountInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace LayerzeroMultitool.Models;
+
+public class AccountInfo
+{
+    public string Address { get; set; }
+    public string PrivateKey { get; set; }
+    public double BinanceSmartChainBalance { get; set; }
+    public double AvalancheBalance { get; set; }
+    public double PolygonBalance { get; set; }
+    public double ArbitrumBalance { get; set; }
+    public double FantomBalance { get; set; }
+    public double UsdtBalance { get; set; }
+    public double TotalBalanceUsd { get; set; }
+}
