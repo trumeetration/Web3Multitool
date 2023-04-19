55e6dbfe66872fb7e09e64a8e2a10ec399e6302c
trum <war.naruto@mail.ru>
2023-04-19 10:14:10 +0500

    [+] Local app config

---
+++ Web3Multitool/AppConfig.cs
@@ -0,0 +1,13 @@
+namespace Web3Multitool.Models;
+
+public class AppConfig
+{
+    public string BinanceAPIKey { get; set; }
+    public string BybitAPIKey { get; set; }
+    public string OKXAPIKey { get; set; }
+    public string ArbitrumRPC { get; set; }
+    public string PolygonRPC { get; set; }
+    public string OptimismRPC { get; set; }
+    public string FantomRPC { get; set; }
+    public string AVAXRPC { get; set; }
+}
