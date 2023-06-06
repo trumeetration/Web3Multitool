bdd769aebdde82c5f64f2cfe4ec1bfabfea85306
trum <war.naruto@mail.ru>
2023-06-06 12:28:55 +0500

    Modified to update UI on update

--- Web3MultiTool.Domain/Models/AccountInfo.cs
+++ Web3MultiTool.Domain/Models/AccountInfo.cs
@@ -1,16 +1,100 @@
-﻿namespace Web3MultiTool.Domain.Models;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-public class AccountInfo
+namespace Web3MultiTool.Domain.Models;
+
+public class AccountInfo : INotifyPropertyChanged
 {
-    public int Id { get; set; }
-    public string Address { get; set; }
-    public string? CexAddress { get; set; }
-    public string PrivateKey { get; set; }
-    public AddressChainInfo FantomInfo { get; set; }
-    public AddressChainInfo AvaxInfo { get; set; }
-    public AddressChainInfo PolygonInfo { get; set; }
-    public AddressChainInfo ArbitrumInfo { get; set; }
-    public AddressChainInfo OptimismInfo { get; set; }
-    public double TotalBalanceUsd { get; set; }
-    public int TotalTxAmount { get; set; }
+    private int _totalTxAmount;
+    private Guid _id;
+    private string _address;
+    private string? _cexAddress;
+    private string _privateKey;
+    private AddressChainInfo _fantomInfo;
+    private AddressChainInfo _avaxInfo;
+    private AddressChainInfo _polygonInfo;
+    private AddressChainInfo _arbitrumInfo;
+    private AddressChainInfo _optimismInfo;
+    private double _totalBalanceUsd;
+
+    public Guid Id
+    {
+        get => _id;
+        set => SetField(ref _id, value);
+    }
+
+    public string Address
+    {
+        get => _address;
+        set => SetField(ref _address, value);
+    }
+
+    public string? CexAddress
+    {
+        get => _cexAddress;
+        set => SetField(ref _cexAddress, value);
+    }
+
+    public string PrivateKey
+    {
+        get => _privateKey;
+        set => SetField(ref _privateKey, value);
+    }
+
+    public AddressChainInfo FantomInfo
+    {
+        get => _fantomInfo;
+        set => SetField(ref _fantomInfo, value);
+    }
+
+    public AddressChainInfo AvaxInfo
+    {
+        get => _avaxInfo;
+        set => SetField(ref _avaxInfo, value);
+    }
+
+    public AddressChainInfo PolygonInfo
+    {
+        get => _polygonInfo;
+        set => SetField(ref _polygonInfo, value);
+    }
+
+    public AddressChainInfo ArbitrumInfo
+    {
+        get => _arbitrumInfo;
+        set => SetField(ref _arbitrumInfo, value);
+    }
+
+    public AddressChainInfo OptimismInfo
+    {
+        get => _optimismInfo;
+        set => SetField(ref _optimismInfo, value);
+    }
+
+    public double TotalBalanceUsd
+    {
+        get => _totalBalanceUsd;
+        set => SetField(ref _totalBalanceUsd, value);
+    }
+
+    public int TotalTxAmount
+    {
+        get => _totalTxAmount;
+        set => SetField(ref _totalTxAmount, value);
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
 }
--- Web3MultiTool.Domain/Models/AddressChainInfo.cs
+++ Web3MultiTool.Domain/Models/AddressChainInfo.cs
@@ -1,11 +1,65 @@
-﻿namespace Web3MultiTool.Domain.Models;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-public class AddressChainInfo
+namespace Web3MultiTool.Domain.Models;
+
+public class AddressChainInfo : INotifyPropertyChanged
 {
-    public int Id { get; set; }
-    public int ChainId { get; set; }
-    public int TxAmount { get; set; }
-    public double BaseBalance { get; set; }
-    public DateTime FirstTxDate { get; set; }
-    public double UsdcBalance { get; set; }
+    private Guid _id;
+    private int _chainId;
+    private int _txAmount;
+    private double _baseBalance;
+    private DateTime _firstTxDate;
+    private double _usdcBalance;
+
+    public Guid Id
+    {
+        get => _id;
+        set => SetField(ref _id, value);
+    }
+
+    public int ChainId
+    {
+        get => _chainId;
+        set => SetField(ref _chainId, value);
+    }
+
+    public int TxAmount
+    {
+        get => _txAmount;
+        set => SetField(ref _txAmount, value);
+    }
+
+    public double BaseBalance
+    {
+        get => _baseBalance;
+        set => SetField(ref _baseBalance, value);
+    }
+
+    public DateTime FirstTxDate
+    {
+        get => _firstTxDate;
+        set => SetField(ref _firstTxDate, value);
+    }
+
+    public double UsdcBalance
+    {
+        get => _usdcBalance;
+        set => SetField(ref _usdcBalance, value);
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
 }
