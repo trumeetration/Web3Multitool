9f44f87717411e09d7c8a0154cfed50f10b65178
trum <war.naruto@mail.ru>
2023-06-06 12:36:22 +0500

    Migrations added

---
+++ Web3Multitool.EntityFramework/Migrations/20230605164627_Initial.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web3Multitool.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AddressChainInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ChainId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TxAmount = table.Column<int>(type: "INTEGER", nullable: false),
+                    BaseBalance = table.Column<double>(type: "REAL", nullable: false),
+                    FirstTxDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UsdcBalance = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AddressChainInfos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AccountInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    CexAddress = table.Column<string>(type: "TEXT", nullable: true),
+                    PrivateKey = table.Column<string>(type: "TEXT", nullable: false),
+                    FantomInfoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AvaxInfoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PolygonInfoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ArbitrumInfoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    OptimismInfoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TotalBalanceUsd = table.Column<double>(type: "REAL", nullable: false),
+                    TotalTxAmount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountInfo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AccountInfo_AddressChainInfos_ArbitrumInfoId",
+                        column: x => x.ArbitrumInfoId,
+                        principalTable: "AddressChainInfos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AccountInfo_AddressChainInfos_AvaxInfoId",
+                        column: x => x.AvaxInfoId,
+                        principalTable: "AddressChainInfos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AccountInfo_AddressChainInfos_FantomInfoId",
+                        column: x => x.FantomInfoId,
+                        principalTable: "AddressChainInfos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AccountInfo_AddressChainInfos_OptimismInfoId",
+                        column: x => x.OptimismInfoId,
+                        principalTable: "AddressChainInfos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AccountInfo_AddressChainInfos_PolygonInfoId",
+                        column: x => x.PolygonInfoId,
+                        principalTable: "AddressChainInfos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountInfo_ArbitrumInfoId",
+                table: "AccountInfo",
+                column: "ArbitrumInfoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountInfo_AvaxInfoId",
+                table: "AccountInfo",
+                column: "AvaxInfoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountInfo_FantomInfoId",
+                table: "AccountInfo",
+                column: "FantomInfoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountInfo_OptimismInfoId",
+                table: "AccountInfo",
+                column: "OptimismInfoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountInfo_PolygonInfoId",
+                table: "AccountInfo",
+                column: "PolygonInfoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountInfo");
+
+            migrationBuilder.DropTable(
+                name: "AddressChainInfos");
+        }
+    }
+}
---
+++ Web3Multitool.EntityFramework/Migrations/20230606065808_DatabaseEdit.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web3Multitool.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabaseEdit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "AddressChainInfos",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PolygonInfoId",
+                table: "AccountInfo",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "OptimismInfoId",
+                table: "AccountInfo",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "FantomInfoId",
+                table: "AccountInfo",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AvaxInfoId",
+                table: "AccountInfo",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ArbitrumInfoId",
+                table: "AccountInfo",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "AccountInfo",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "AddressChainInfos",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PolygonInfoId",
+                table: "AccountInfo",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OptimismInfoId",
+                table: "AccountInfo",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FantomInfoId",
+                table: "AccountInfo",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AvaxInfoId",
+                table: "AccountInfo",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ArbitrumInfoId",
+                table: "AccountInfo",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "AccountInfo",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
