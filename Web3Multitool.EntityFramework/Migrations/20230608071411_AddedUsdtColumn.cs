599561700bfffd4a09eaf77cf48340982c336e09
trum <war.naruto@mail.ru>
2023-06-09 03:03:28 +0500

    migrations for usdt column

---
+++ Web3Multitool.EntityFramework/Migrations/20230608071411_AddedUsdtColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web3Multitool.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUsdtColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "UsdtBalance",
+                table: "AddressChainInfos",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsdtBalance",
+                table: "AddressChainInfos");
+        }
+    }
+}
