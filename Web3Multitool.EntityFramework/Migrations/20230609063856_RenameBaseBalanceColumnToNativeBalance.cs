f9f4d4ad27a865017552e3d669dc3d0427f6ed48
trum <war.naruto@mail.ru>
2023-06-09 23:07:54 +0500

    Renamed column BaseBalance to NativeBalance

---
+++ Web3Multitool.EntityFramework/Migrations/20230609063856_RenameBaseBalanceColumnToNativeBalance.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web3Multitool.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameBaseBalanceColumnToNativeBalance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BaseBalance",
+                table: "AddressChainInfos",
+                newName: "NativeBalance");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NativeBalance",
+                table: "AddressChainInfos",
+                newName: "BaseBalance");
+        }
+    }
+}
