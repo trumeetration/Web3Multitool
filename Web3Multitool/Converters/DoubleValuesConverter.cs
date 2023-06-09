94a50eea337ce14ad4496ac06192e6360dbae08e
trum <war.naruto@mail.ru>
2023-06-09 23:10:33 +0500

    Converter for double values

---
+++ Web3Multitool/Converters/DoubleValuesConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Web3Multitool.Converters;
+
+public class DoubleValuesConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+    {
+        try
+        {
+            return value == null ? "0" : $"{value:N2}";
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine(e);
+            return "Invalid";
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
